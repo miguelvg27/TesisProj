2f0d3d22188bd3b3bd166909d67a23bf60a3d786
Pedro <curichpedro@gmail.com>
2013-07-07 18:36:21 -0500

    Avance de Simulacion

---
+++ TesisProj/Areas/Simulaciones/Controllers/MaestroSimulacionController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesisProj.Areas.Modelo.Models;
+using TesisProj.Areas.Modelos.Models;
+using TesisProj.Areas.Simulaciones.Models;
+using TesisProj.Models;
+using TesisProj.Models.Storage;
+
+namespace TesisProj.Areas.Simulaciones.Controllers
+{
+    public class MaestroSimulacionController : Controller
+    {
+        //
+        // GET: /Simulaciones/MaestroSimulacion/
+        TProjContext context = new TProjContext();
+
+        public ActionResult Index(int idProyecto)
+        {
+            var elementos = context.Elementos.Include("Parametros").Where(e => e.IdProyecto == idProyecto).ToList();
+            List<MaestroSimulacion> salida = new List<MaestroSimulacion>();
+
+            foreach (Elemento elemento in elementos)
+            {
+                var parametros = elemento.Parametros;
+
+                foreach (Parametro parametro in parametros)
+                {
+                    if (parametro.Sensible)
+                    {
+                        ModeloSimlacion m = new ModeloSimlacion();
+                        double mean =parametro.Celdas.Average(e =>Convert.ToDouble(e.Valor));
+                        double std =Calculos.DesviacionStandard(parametro.Celdas.Select(e => Convert.ToDouble(e.Valor)).ToList());
+                        m.Normal= new Normal(mean,std);
+                        MaestroSimulacion aux = new MaestroSimulacion(parametro, m);
+                        salida.Add(aux);
+                    }
+                }
+            }
+
+            return View(salida);
+        }
+
+    }
+}
---
+++ TesisProj/Areas/Simulaciones/Models/MaestroSimulacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TesisProj.Areas.Modelo.Models;
+using TesisProj.Areas.Modelos.Models;
+
+namespace TesisProj.Areas.Simulaciones.Models
+{
+    public class MaestroSimulacion
+    {
+        public virtual Parametro parametro { get; set; }
+        public virtual ModeloSimlacion modelobase { get; set; }
+
+        public MaestroSimulacion(Parametro p, ModeloSimlacion m)
+        {
+            this.parametro = p;
+            this.modelobase = m;
+        }
+
+        public void ActualizarCeldas(string modelo)
+        {
+            if (modelobase.Nombre.Equals("Normal"))
+            {
+                foreach (Celda c in parametro.Celdas)
+                {
+                    RandomGenerator rg = new RandomGenerator(new Random());
+                    c.Valor = Convert.ToDecimal(rg.NormalDeviate()+modelobase.Normal.mean);
+                }
+            }
+        }
+
+        public List<Celda> GeteldasSimuladas()
+        {
+            return parametro.Celdas;
+        }
+    }
+}
