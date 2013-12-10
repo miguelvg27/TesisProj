af1381e01ad4473c4e5e7a5467f37adad3df0464
Pedro <curichpedro@gmail.com>
2013-12-09 23:59:10 -0500

    alter3

--- TesisProj/Models/Storage/TProjContext.cs
+++ TesisProj/Models/Storage/TProjContext.cs
@@ -19,51 +19,51 @@ namespace TesisProj.Models.Storage
             //  : base("TProjContext")
            : base("Server=b2b54485-7ca7-43cc-9153-a28f004c1a40.sqlserver.sequelizer.com;Database=dbb2b544857ca743cc9153a28f004c1a40;User ID=bmprfcrbhkmmbkoa;Password=LybRmMNWAWF7BLMuV56FWdhmSZRF4PYFqLawYrGxYVpxHSHNUiQhAbmHqX7u5g2T;")
         {
-            RegistrarTablas();
+            //RegistrarTablas();
         }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Conventions.Remove<IncludeMetadataConvention>(); 
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
-            modelBuilder.Conventions.Remove<OneToOneConstraintIntroductionConvention>(); 
-            modelBuilder.Entity<Celda>().Property(c => c.Valor).HasPrecision(16, 5);
-            modelBuilder.Entity<ListField>().Property(p => p.Imagen).HasColumnType("Image");
-        }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    //modelBuilder.Conventions.Remove<IncludeMetadataConvention>(); 
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //    modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        //    modelBuilder.Conventions.Remove<OneToOneConstraintIntroductionConvention>(); 
+        //    modelBuilder.Entity<Celda>().Property(c => c.Valor).HasPrecision(16, 5);
+        //    modelBuilder.Entity<ListField>().Property(p => p.Imagen).HasColumnType("Image");
+        //}
 
-        public void RegistrarTablas()
-        {
-            RegistrarTablasPlantilla();
-            RegistrarTablasProyecto();
-            RegistrarTablaImagenes();
+        //public void RegistrarTablas()
+        //{
+        //    RegistrarTablasPlantilla();
+        //    RegistrarTablasProyecto();
+        //    RegistrarTablaImagenes();
 
-        }
+        //}
 
-        public void Seed()
-        {
-            SeedPlantilla();
-            SeedUserProfiles();
-            SeedProyecto();
-            SeedTablaImagenes();
-        }
+        //public void Seed()
+        //{
+        //    SeedPlantilla();
+        //    SeedUserProfiles();
+        //    SeedProyecto();
+        //    SeedTablaImagenes();
+        //}
     }
 
-    public class TProjInitializer : CreateDatabaseIfNotExists<TProjContext>
-    //public class TProjInitializer:  DropCreateDatabaseIfModelChanges<TProjContext>
-    //public class TProjInitializer : DropCreateDatabaseAlways<TProjContext>
-    {
-        protected override void Seed(TProjContext context)
-        {
-            context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaOperacion ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaParametro ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaFormula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE TipoFormula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE Operacion ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE Parametro ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
-            context.Database.ExecuteSqlCommand("ALTER TABLE Formula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //public class TProjInitializer : CreateDatabaseIfNotExists<TProjContext>
+    ////public class TProjInitializer:  DropCreateDatabaseIfModelChanges<TProjContext>
+    ////public class TProjInitializer : DropCreateDatabaseAlways<TProjContext>
+    //{
+    //    protected override void Seed(TProjContext context)
+    //    {
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaOperacion ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaParametro ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE PlantillaFormula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE TipoFormula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE Operacion ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE Parametro ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
+    //        context.Database.ExecuteSqlCommand("ALTER TABLE Formula ALTER COLUMN Referencia VARCHAR(255) COLLATE SQL_Latin1_General_CP1_CS_AS NULL");
 
-            context.Seed();
-        }
-    }
+    //        context.Seed();
+    //    }
+    //}
 }
